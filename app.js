5246149682ab4d9f411cca9ea18aa7c96981d01e
fantasy150592 <pdphuongit@gmail.com>
2019-01-16 23:04:09 +0700

    change

---
+++ app.js
@@ -0,0 +1,10 @@
+// hide preloader
+// all the images scripts links have finished loading
+window.addEventListener('load', function() {
+    document.querySelector('.preloader').style.display = "none";
+});
+
+//nav btn
+document.querySelector('.navBtn').addEventListener('click', function() {
+    document.querySelector('.nav');
+});
